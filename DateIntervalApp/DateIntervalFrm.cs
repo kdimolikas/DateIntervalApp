23cea580462866cc33759f0520ed50aeb21ec931
kdimolikas <konstdimolikas@gmail.com>
2019-11-26 20:35:48 +0200

    Update methods of UI

--- DateIntervalApp/DateIntervalFrm.cs
+++ DateIntervalApp/DateIntervalFrm.cs
@@ -1,89 +1,132 @@
 ﻿using System;
-
+using System.ComponentModel;
 using System.Windows.Forms;
 
 
 namespace DateIntervalApp
 {
-
-
+    
     /// <summary>
     /// Contains the event handlers of the application. 
     /// </summary>
     public partial class DateIntervalFrm : Form
     {
         
-
-        private DateIntervalManager manager;
-
-
+        private DateIntervalManager interval;
+        private static float FONT_SIZE = 26.0F;
 
         public DateIntervalFrm()
         {
             InitializeComponent();
-            manager = new DateIntervalManager(dateTimePicker1.Value, dateTimePicker2.Value);
+            /*Setup event handlers*/
+            NumericSubtractYears.ValueChanged += Numeric_ValueChanged;
+            NumericSubtractMonths.ValueChanged += Numeric_ValueChanged;
+            NumericSubtractDays.ValueChanged += Numeric_ValueChanged;
+            NumericAddYears.ValueChanged += Numeric_ValueChanged;
+            NumericAddMonths.ValueChanged += Numeric_ValueChanged;
+            NumericAddDays.ValueChanged += Numeric_ValueChanged;
+            dateTimePicker1.ValueChanged += Numeric_ValueChanged;
+            dateTimePicker2.ValueChanged += Numeric_ValueChanged;
+            
+            NumericAddYears.Validating += Numeric_Validating;
+            NumericAddMonths.Validating += Numeric_Validating;
+            NumericAddDays.Validating += Numeric_Validating;
+            NumericSubtractYears.Validating += Numeric_Validating;
+            dateTimePicker1.Validating += Numeric_Validating;
+            dateTimePicker2.Validating += Numeric_Validating;
 
+            interval = new DateIntervalManager(dateTimePicker1.Value, dateTimePicker2.Value);
+            interval.SetDateInterval();
+
+            this.DisplayInterval();
+            this.UpdateLimits();
         }
                       
-
         /// <summary>
         /// <c>DisplayInterval</c> method displays the interval between two dates.
         /// </summary>
         private void DisplayInterval()
+        {   
+            DisplayYears();
+            DisplayMonths();
+            DisplayDays();
+        }    
+
+        private void DisplayYears()
         {
-
-            textBox1.AppendText("**** Time elapsed ****"+System.Environment.NewLine);
-            textBox1.AppendText("Years: "+ manager.GetDateInterval().years+ System.Environment.NewLine);
-            textBox1.AppendText("Months: "+ manager.GetDateInterval().months+ System.Environment.NewLine);
-            textBox1.AppendText("Days: "+ manager.GetDateInterval().days+ System.Environment.NewLine);
-            textBox1.AppendText("**** Time elapsed ****");
-
-
+            TextBoxYears.Clear();
+            TextBoxYears.SelectionAlignment = HorizontalAlignment.Right;
+            TextBoxYears.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, 8);
+            TextBoxYears.AppendText("years" + Environment.NewLine);
+            TextBoxYears.SelectionAlignment = HorizontalAlignment.Center;
+            TextBoxYears.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, FONT_SIZE);
+            TextBoxYears.AppendText(interval.Years.ToString());
         }
-        
-               
 
+        private void DisplayMonths()
+        {
+            TextBoxMonths.Clear();
+            TextBoxMonths.SelectionAlignment = HorizontalAlignment.Right;
+            TextBoxMonths.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, 8);
+            TextBoxMonths.AppendText("months" + Environment.NewLine);
+            TextBoxMonths.SelectionAlignment = HorizontalAlignment.Center;
+            TextBoxMonths.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, FONT_SIZE);
+            TextBoxMonths.AppendText(interval.Months.ToString());
+        }
+
+        private void DisplayDays()
+        {
+            TextBoxDays.Clear();
+            TextBoxDays.SelectionAlignment = HorizontalAlignment.Right;
+            TextBoxDays.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, 8);
+            TextBoxDays.AppendText("days" + Environment.NewLine);
+            TextBoxDays.SelectionAlignment = HorizontalAlignment.Center;
+            TextBoxDays.SelectionFont = new System.Drawing.Font(Font.OriginalFontName, FONT_SIZE);
+            TextBoxDays.AppendText(interval.Days.ToString());
+        }
+
+        /// <summary>
+        /// Update numeric max and min limits 
+        /// </summary>
+        private void UpdateLimits()
+        {
+            NumericSubtractYears.Maximum = interval.Years 
+                                           + NumericAddYears.Value
+                                           - NumericSubtractYears.Value;
+            NumericSubtractYears.Minimum = 0;
+        }
 
         //*****************Event handlers***************//
-
-        //Go button
-        private void goBtn_Click(object sender, EventArgs e)
+        private void Numeric_ValueChanged(object sender, EventArgs e)
         {
-            textBox1.Clear();
-           
-            manager.SetDateInterval();
-            manager.AddTime(Convert.ToInt32(numericUpDown1.Value),
-                Convert.ToInt32(numericUpDown2.Value),
-                Convert.ToInt32(numericUpDown3.Value));
-            manager.SubtractTime(Convert.ToInt32(numericUpDown4.Value),
-                 Convert.ToInt32(numericUpDown5.Value),
-                 Convert.ToInt32(numericUpDown6.Value));
-
-            this.DisplayInterval();
-
+            try
+            {
+                interval.SetFirstDate(dateTimePicker1.Value);
+                interval.SetSecondDate(dateTimePicker2.Value);
+                interval.AddTime(Decimal.ToInt32(NumericAddYears.Value),
+                                 Decimal.ToInt32(NumericAddMonths.Value),
+                                 Decimal.ToInt32(NumericAddDays.Value));
+                interval.SubtractTime(Decimal.ToInt32(NumericSubtractYears.Value),
+                                      Decimal.ToInt32(NumericSubtractMonths.Value),
+                                      Decimal.ToInt32(NumericSubtractDays.Value));
+                this.DisplayInterval(); 
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Out of range", "Range Exception", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
-
-        //Clear button
-        private void clearBtn_Click(object sender, EventArgs e)
+        private void Numeric_Validating(object sender, CancelEventArgs e)
         {
-            textBox1.Clear();
+            try
+            {
+                UpdateLimits();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                e.Cancel = true;
+            }     
         }
-
-
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-            manager.SetDate1(dateTimePicker1.Value);
-        }
-
-
-
-        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
-        {
-            manager.SetDate2(dateTimePicker2.Value);
-        }
-
-
     }
 }
